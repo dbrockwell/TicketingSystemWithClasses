c610a54247f471729f8f0630a46e6e2a52471d93
David Brockwell <dbrockwell@my.wctc.edu>
2022-02-26 14:13:16 -0600

    Added another get set and added return statment

--- tickets.cs
+++ tickets.cs
@@ -1,12 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ticketsV2 {
     public class tickets {
-        public UInt64 ticketId { get; set; }
+        public UInt64 ticketID { get; set; }
         public string summary {get; set;}
         public string status {get; set;}
         public string priority {get; set;}
         public string submitter {get; set;}
         public string assigned {get; set;}
+        public List<string> peopleWatching {get; set;}
+
+        public string entry() {
+            string peopleWatchingString = "";
+            string lastperson = peopleWatching.LastOrDefault();
+            foreach (string person in peopleWatching) {
+                if (person.Equals(lastperson)) {
+                    peopleWatchingString += person;
+                }
+                else {
+                    peopleWatchingString += person + "|";
+                }
+            }
+            return $"{ticketID},{summary},{status},{priority},{submitter},{assigned},{peopleWatchingString}";
+        }
     }
 }
