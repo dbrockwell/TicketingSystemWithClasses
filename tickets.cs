f89cc2aba05e0fd8b043b048ab107bfa778e049a
David Brockwell <dbrockwell@my.wctc.edu>
2022-02-26 13:55:26 -0600

    added majority of getters and setters

--- tickets.cs
+++ tickets.cs
@@ -2,6 +2,11 @@ using System;
 
 namespace ticketsV2 {
     public class tickets {
-
+        public UInt64 ticketId { get; set; }
+        public string summary {get; set;}
+        public string status {get; set;}
+        public string priority {get; set;}
+        public string submitter {get; set;}
+        public string assigned {get; set;}
     }
 }
