314a35d57cc150922d737018f5b63f0e9e354856
David Brockwell <dbrockwell@my.wctc.edu>
2022-02-26 14:32:34 -0600

    Added getters and setters with nlog

--- ticketfile.cs
+++ ticketfile.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using NLog.Web;
 
 namespace ticketsV2 {
     public class ticketfile {
-
+        public string filePath {get; set;}
+        public List<tickets> Tickets {get; set;}
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
     }
 }
