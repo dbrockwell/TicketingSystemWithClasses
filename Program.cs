d3ae83b066facff5ddefac2ef0edbddfdb5b2a1e
David Brockwell <dbrockwell@my.wctc.edu>
2022-02-27 01:00:35 -0600

    Started main file

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.IO;
+using NLog.Web;
 
 namespace ticketsV2
 {
     class Program
     {
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string ticketFilePath = Directory.GetCurrentDirectory() + "\\tickets.csv";
+            logger.Info("Program started");
+            TicketFile ticketsFile = new TicketFile(ticketFilePath);
         }
     }
 }
