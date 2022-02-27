16b4973dbb41c3dbd22dfa1f732ce24dfd1b8011
David Brockwell <dbrockwell@my.wctc.edu>
2022-02-27 00:01:53 -0600

    Ability to read file

--- ticketfile.cs
+++ ticketfile.cs
@@ -6,7 +6,24 @@ using NLog.Web;
 namespace ticketsV2 {
     public class ticketfile {
         public string filePath {get; set;}
-        public List<tickets> Tickets {get; set;}
+        public List<string> Tickets {get; set;}
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+
+        public ticketfile(string ticketFilePath) {
+            filePath = ticketFilePath;
+            Tickets = new List<string>();
+            if(File.Exists(filePath))
+            {
+                StreamReader ticketRead = new StreamReader(filePath);
+                while (!ticketRead.EndOfStream)
+                {
+                    Tickets.Add(ticketRead.ReadLine());
+                }
+            }
+            else
+            {
+                logger.Error("File Does Not Exist");
+            }
+        }
     }
 }
